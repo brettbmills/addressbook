c37e89f13c1d248e825c4e2f08aba52930b23ecf
brettbmills <46800517+brettbmills@users.noreply.github.com>
2020-07-30 15:05:13 -0600

    Add files via upload

---
+++ app/models/contact.rb
@@ -0,0 +1,5 @@
+class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true, length: { minimum: 10, maximum: 10}
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # give user their contact
+  has_many :contact
+
+end
